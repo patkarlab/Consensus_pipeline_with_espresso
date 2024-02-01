eac9cbe69d1dfb2330df068fbb7759ea4127d401
patkarlab <nvpatkar@gmail.com>
2024-02-01 09:54:45 +0530

    updated bedfile

--- run_nextflow.sh
+++ run_nextflow.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/bash
-##BSUB -J smMIPS_pipeline
-##BSUB -n 25
-##BSUB -q normal
-##-m cn2" to submit jobs to cn2
-## or " -m cn3"
-
 ##########
 # Original bedfiles 
 # bedfile: /home/pipelines/Consensus_pipeline_with_espresso/bedfiles/Probes-XGEN_npm1_cebpa_phf6_sortd, bedfile2: /home/pipelines/Consensus_pipeline_with_espresso/bedfiles/AML_MRD_Exons_sortd
 
 # Updated bedfiles 22Dec23
 # bedfile: /home/pipelines/Consensus_pipeline_with_espresso/bedfiles/Probes-XGEN_npm1_cebpa_phf6_updated_uniq_sortd, bedfile2: /home/pipelines/Consensus_pipeline_with_espresso/bedfiles/AML_MRD_Exons_updated_sortd
+
+# Updated bedfile 24Jan24
+# bedfile: /home/pipelines/Consensus_pipeline_with_espresso/bedfiles/Probes-XGEN_24jan24_sortd, bedfile2: /home/pipelines/Consensus_pipeline_with_espresso/bedfiles/AML_MRD_Exons_sortd
 
 source activate new_base
 nextflow -c /home/pipelines/Consensus_pipeline_with_espresso/nextflow.config run main.nf -entry MRD \
